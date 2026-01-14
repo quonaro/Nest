57f2562ceae5b0260870c1fd7e8ac3817c2bc236
quonaro <quonaro@mail.ru>
2026-01-14 09:21:09 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === 'production' ? '/Nest/' : '/'
+  base: process.env.NODE_ENV === 'production' ? '/' : '/'
 })
 
